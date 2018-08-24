21e769b602d48b725348b7a125e4aed1f80fcd60
Christopher Clarke <afroradiohead@gmail.com>
2018-08-24 14:24:45 -0700

    test port

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,12 +4,13 @@ import {AppService} from './app.service';
 import {OrdersController} from './api/v1/orders.controller';
 import {TypeOrmModule} from '@nestjs/typeorm';
 
+console.log(process.env.DB_HOST, +(process.env.DB_PORT));
 @Module({
   imports: [
       TypeOrmModule.forRoot({
           type: 'mysql',
           host: process.env.DB_HOST,
-          port: +process.env.DB_PORT,
+          port: +(process.env.DB_PORT),
           username: process.env.DB_USERNAME,
           password: process.env.DB_PASSWORD,
           database: process.env.DB_DATABASE,
--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import {AppModule} from './app.module';
 import * as exphbs from 'express-handlebars';
 import * as path from 'path';
 async function bootstrap() {
-const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule);
   app.engine('handlebars', exphbs());
   app.set('view engine', 'handlebars');
   app.set('views', path.join(__dirname, 'views'));
